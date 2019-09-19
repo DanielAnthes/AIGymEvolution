6a5f0a58b6dadd01783c92d7ba3c1b34e16c91c0
DanielAnthes <daniel.anthes97@gmail.com>
2019-09-20 01:06:38 +0200

    non learning evolutionary algorithm

--- cartpole_evolution.py
+++ cartpole_evolution.py
@@ -4,19 +4,20 @@ import matplotlib.pyplot as plt
 from random import choice
 
 env = gym.make('CartPole-v0')           # initialize environment
-num_episodes = 20                       # number of episodes
-pop_size = 30
-weights = np.random.rand(4,pop_size) * 2 - 1  # randomly initialize parameters
 
 # each agents' weights are stored as a column in the weight matrix, randomly
 # recombine weights and create new agents
 def create_new_generation(pop_size, best_agents):
     num_params = best_agents.shape[0]
-    new_pop = np.zeros(num_params, pop_size)
+    new_pop = np.zeros([num_params, pop_size])
 
+    '''                             randomly shuffle parameters from different good agents
     for a in range(pop_size):
-        for p in num_params:
-            new_pop[p,a] = choice(best_agents[p,:])
+        for p in range(num_params):
+            new_pop[p,a] = np.random.normal(loc=choice(best_agents[p,:]), scale=0.3)
+    '''
+    for a in range(pop_size):
+        new_pop[:,a] = [np.random.normal(loc=x, scale=0.3) for x in choice(best_agents.T).T]
 
     return new_pop
 
@@ -31,7 +32,7 @@ def run_episode(env, parameters):
     observation = env.reset()
     totalreward = 0
     for _ in range(200):
-        action = calc_action(parameters, weights)
+        action = calc_action(parameters, parameters)
         observation, reward, done, info = env.step(action)
         totalreward += reward
         if done:
@@ -50,23 +51,39 @@ def run_generation(env, parameters):
 
     best_reward = np.max(rewards)
     mean_reward = np.mean(rewards)
-    print("Best reward in generation: ", best_reward)
-    print("Mean reward in generation: ", mean_reward)
 
     ranking = rewards.argsort()
-    best_weights = parameters[:,ranking[-5:]] # matrix with 5 best agents
+    return parameters[:,ranking[-5:]], best_reward, mean_reward
 
 
-# run some episodes with random weights to test
-rewards = np.zeros(num_episodes)
-for i in range(num_episodes):
-    print("*** EPISODE ", i, " ***")
-    rewards[i] = run_episode(env, weights)
+def run(env, num_episodes=200, pop_size=30):
+    parameters = np.random.rand(4,pop_size) * 2 - 1  # randomly initialize parameters
+    best_rewards = np.zeros(num_episodes)
+    mean_rewards = np.zeros(num_episodes)
 
-print(rewards)
+    for i in range(num_episodes):
+        print("*** EPISODE ", i, " ***")
+        # run simulation for every agent in population
+        best_agents, best_reward, mean_reward = run_generation(env, parameters)
+        best_rewards[i] = best_reward
+        mean_rewards[i] = mean_reward
+        print("Best reward in generation: ", best_reward)
+        print("Mean reward in generation: ", mean_reward)
 
-plt.figure()
-plt.plot(rewards)
-plt.xlab("episode")
-plt.ylab("reward")
-plt.show()
+        # generate new population
+        weights = create_new_generation(pop_size, best_agents)
+
+    print("done training")
+    return weights, best_rewards, mean_rewards
+
+def plot_training(best_rewards, mean_rewards):
+    plt.figure()
+    plt.plot(best_rewards)
+    plt.plot(mean_rewards)
+    plt.legend(["best score", "mean score"])
+    plt.xlabel("Episode")
+    plt.ylabel("Score")
+    plt.show()
+
+weights, best_scores, mean_scores = run(env, 10000, 100)
+plot_training(best_scores, mean_scores)
